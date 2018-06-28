a0b603b0980f400d88a42bb0792b49050f9b8347
Nanda Kumar <Nanda Kumar>
2018-06-28 23:25:30 +0530

    code updated

--- server/controller/RouteController.js
+++ server/controller/RouteController.js
@@ -11,33 +11,37 @@ exports.PostLoc=function(req,res){
 	var isStarted=reqBody.isStarted;
 	var isCompleted=reqBody.isCompleted;
 	var coordinates=reqBody.coordinates;
-	console.log("hitted");
-	if(isStarted==true){
+	console.log("hitted");	
 		Routes.findOne({tripId:tripId},function(err,routeData){
-	if(err){
+		if(err){
 		res.send("FindErr",err)
-	}
-	if(routeData==null){
+		}
+		if(routeData==null){
 		console.log("tripId",tripId)
-		var saveData={};			
-		saveData.tripId=tripId;
-		saveData.routes=[coordinates];			
-		console.log("saveData",saveData)
+		if(isStarted==true){
+			var saveData={};			
+			saveData.tripId=tripId;
+			saveData.routes=[coordinates];			
+			console.log("saveData",saveData)
+			
+			var SaveObj=Routes(saveData);
+			
+			SaveObj.save(function(err,saved){
+				res.send(saved)
+				console.log("saved",saved)
+			})
+		}			
+		else{
+			res.send("Trip Not Started")
+		}			
 		
-		var SaveObj=Routes(saveData);
-		
-		SaveObj.save(function(err,saved){
-			res.send("saved")
-			console.log("saved",saved)
-		})
-		
-	}else{			
+		}else{			
 		if(isCompleted==true){
 			routeData.isCompleted=true;
 			routeData.routes.push(coordinates)
 			routeData.completedAt=new Date();				
 			routeData.save(function(err,saved){
-			res.send("saved")
+			res.send(saved)
 			console.log("err,saved",saved)
 			})
 		}else{
@@ -48,21 +52,17 @@ exports.PostLoc=function(req,res){
 			}else{
 				routeData.routes.push(coordinates)
 				routeData.save(function(err,saved){
-				res.send("saved")
+				res.send(saved)
 				console.log("err,saved",saved)
 			})
 			
 		}
 		}
 	}
-	});
-	}
-	else{
-		res.send("Trip Not Started")
-	}	
+	});	
 }
 
-exports.PostLoc=function(req,res){	
+exports.getLoc=function(req,res){	
 	var tripId=req.params.tripId;	
 	Routes.findOne({tripId:tripId},function(err,routeData){
 	if(err){
--- server/route.js
+++ server/route.js
@@ -7,5 +7,5 @@ router.get('/api/funapp/home',saveUser.home);
 router.get('/api/funapp/login',saveUser.login)
 router.post('/api/funapp/view',saveUser.view)
 router.post('/api/route/postloc',Route.PostLoc)
-router.get('/api/route/:tripId',Route.PostLoc)
+router.get('/api/route/:tripId',Route.getLoc)
 }
